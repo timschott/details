8c53d6bd19ff349f8fbafefd2f21cb270ed49543
timschott <tcs9pk@virginia.edu>
2021-10-28 18:18:40 -0700

    header

--- get_26_gutentexts.sh
+++ get_26_gutentexts.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 allIds=(1342 161 1023 1400 219 507 2413 4276 1399 6593 521 33 174 35 103 164 84 11 64317 2554 2130 145 140 1156 5135 18247)
 allNames=(pride_and_prejudice sense_and_sensibility bleak_house great_expectations heart_of_darkness adam_bede madame_bovary north_and_south anna_karenina tom_jones robinson_crusoe scarlet_letter picture_of_dorian_grey the_time_machine around_the_world_in_80_days twenty_thousand_leagues_under_the_sea frankenstein alice_in_wonderland great_gatsby crime_and_punishment utopia middlemarch the_jungle babbit the_fortune_of_the_rougons the_last_man)
 
