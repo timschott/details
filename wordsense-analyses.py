1cd8d0c672d060b230467b2b871ea2ffbaa1a63f
timschott <tcs9pk@virginia.edu>
2022-10-20 14:13:41 -0700

    update neural specificity method

--- wordsense-analyses.py
+++ wordsense-analyses.py
@@ -8,6 +8,9 @@ from transformers_encoder import TransformersEncoder
 from vectorspace import SensesVSM
 from wn_utils import WN_Utils
 
+from nltk.corpus import wordnet as wn
+from nltk.corpus.reader.wordnet import WordNetError
+
 '''
 Helper method to load a pretrained model from Loureiro and Jorge (2019)
 This model is trained to disambiguate wordsenses, performing better than more rudimentary options
@@ -112,16 +115,20 @@ def disambiguate_lemmas(lemmas, idxs, doc, target_pos, wsd_encoder, senses_vsm):
 
 	return nouns, deps, lexs
 
-def specificity_neural(sample, spacy_tagger, target_pos):
-	print(sample)
+def specificity_neural(sample, spacy_tagger, wsd_encoder, senses_vsm, target_pos_list):
 	tagged_sample=spacy_tagger(sample)
 	hyper_sum = 0
 	noun_and_verb_count = 0
+	target_pos = []
 
 	target_idxs = []
 	for i, word in enumerate(tagged_sample):
-		if word.pos_ in target_pos:
+		if word.pos_ in target_pos_list:
+			target_pos.append(word.pos_)
 			target_idxs.append(i)
+			noun_and_verb_count += 1
+
+	target_lemmas = [tagged_sample[i].lemma_ for i in target_idxs]
 
 	# retrieve contextual embedding for target token/span
 	tokens = [t.text for t in tagged_sample]
@@ -129,25 +136,87 @@ def specificity_neural(sample, spacy_tagger, target_pos):
 	contextual_embeddings = wsd_encoder.token_embeddings([tokens])[0]
 
 	# get target embedding
-	for i in range(len(idxs)):
+	for i in range(len(target_idxs)):
 		# get target embedding
-		target_embedding = get_target_embedding(contextual_embeddings, idxs[i])
+		target_embedding = get_target_embedding(contextual_embeddings, target_idxs[i])
 
 		# perform lookup
-		matches = senses_vsm.match_senses(target_embedding, lemma=lemmas[i], postag=target_pos, topn=1)
+		matches = senses_vsm.match_senses(target_embedding, lemma=target_lemmas[i], postag=target_pos[i], topn=1)
 
+		# if empty, continue
 		if matches is None:
 			noun_and_verb_count -= 1
 			continue
 
+		# iterate through (single val) tuple
 		for sk, sim in matches:
-			syn = wn_utils.sk2syn(sk)
-			hyper_sum += len(list(syn.closure(syn.hypernyms())))
 
-	# a few 'descriptive' passages lack 
+			# get proper synset
+			syn = wn_utils.sk2syn(sk)
+
+			# update hyper_sum with the number of nouns in the hypernym chain for that lemma's synset
+			hyper_sum += len(list(syn.closure(lambda s: s.hypernyms())))
+
+	# a few 'descriptive' passages lack any actual nouns, control for that
 	if noun_and_verb_count == 0:
 		return 0
+
 	return hyper_sum / noun_and_verb_count
+
+'''
+Consult wordnet for the situation of a noun and verb with respect to its station in the hypernym hierarchy. 
+Based on current SOA, it is acceptable to simply grab the top-level (.01) synset.
+
+Args:
+    tagged_sample: a spacy doc
+
+Return:
+    specificity: a value conveying the "specificity" of the input, via Nelson (2020)
+'''
+
+def specificity_normal(sample, spacy_tagger, passage_id):
+
+    tagged_sample=spacy_tagger(sample)
+    hyper_sum = 0
+    noun_and_verb_count = 0
+    for word in tagged_sample:
+        if word.pos_ == "NOUN" or word.pos_ == "VERB":
+            noun_and_verb_count +=1
+            # if it's a verb, get the most common verb hypernym chain
+            # else, get the most common noun hypernym chain
+            pos = word.pos_
+            tag = "n" if pos.startswith("N") else "v"
+            synset = word.lemma_ + "." + tag + ".01"    
+            # occasionally, an adjective will be misidentified as a noun;
+            # in that case, catch the exception (since the word won't a true synset) + fix the count
+            try: 
+                synset_def = wn.synset(synset)
+                # print(f"noun: {word.lemma_}, first_syn: {synset}, calculated syn: {synset_def}")
+            except WordNetError as w_e:
+                noun_and_verb_count -= 1
+                continue
+            else:
+                hyper_sum +=len(list(synset_def.closure(lambda s: s.hypernyms())))
+
+    # a few 'descriptive' passages lack 
+    if noun_and_verb_count == 0:
+        return 0
+    return hyper_sum / noun_and_verb_count
+
+'''
+Helper for reporting 5-number summary of an inputted list
+'''
+def five_number(data):
+    # calculate quartiles
+    quartiles = np.percentile(data, [25, 50, 75])
+    # calculate min/max
+    data_min, data_max = data.min(), data.max()
+    # print 5-number summary
+    print('Min: %.3f' % data_min)
+    print('Q1: %.3f' % quartiles[0])
+    print('Median: %.3f' % quartiles[1])
+    print('Q3: %.3f' % quartiles[2])
+    print('Max: %.3f' % data_max)
 
 if __name__ == '__main__':
 
@@ -162,6 +231,8 @@ if __name__ == '__main__':
 	all_nouns = []
 	all_deps = []
 	all_lexs = []
+	test_specs = []
+	old_specs = []
 	c = 0
 	for passage in descriptive_passages['passage']:
 		passage_nouns = []
@@ -171,15 +242,20 @@ if __name__ == '__main__':
 		all_nouns.append(passage_nouns)
 		all_deps.append(passage_deps)
 		all_lexs.append(passage_lexs)
-		test_spec = specificity_neural(passage, en_nlp, ["NOUN", "VERB"])
+		test_spec = specificity_neural(passage, en_nlp, encoder, vsm, ["NOUN", "VERB"])
+		test_specs.append(test_spec)
+		old_spec = specificity_normal(passage, en_nlp, descriptive_passages['passage_id'].iloc[c])
+		old_specs.append(old_spec)
 		if c % 100 == 0 and c != 0:
-			break
 			print(f"inventorying passage at index {c} with id {descriptive_passages['passage_id'].iloc[c]}")
 		c+=1
 
 	print(f"inventoried {c} passages")
 
-	descriptive_passages['nouns'] = all_nouns
-	descriptive_passages['deps'] = all_deps
-	descriptive_passages['supersenses'] = all_lexs
+	# descriptive_passages['nouns'] = all_nouns
+	# descriptive_passages['deps'] = all_deps
+	# descriptive_passages['supersenses'] = all_lexs
+	five_number(test_specs)
+	print("------------")
+	five_number(old_specs)
 
