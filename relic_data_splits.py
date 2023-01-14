119d6e28e777a84f89f81abaf363a5432d966d2f
timschott <tcs9pk@virginia.edu>
2023-01-13 21:15:28 -0800

    clean up

--- relic_data_splits.py
+++ relic_data_splits.py
@@ -23,11 +23,6 @@ import random
 from collections import Counter
 
 # sklearn helpers
-from sklearn.metrics import accuracy_score 
-from sklearn.metrics import confusion_matrix 
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_score 
-from sklearn.metrics import recall_score 
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
